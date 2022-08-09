0e77c6388160664f89d28d968b66ddb27e79fc94
Wellison Martins <well.lucaas@gmail.com.br>
2022-08-09 14:58:53 -0300

    att 09-08 14:58

--- ota/util.py
+++ ota/util.py
@@ -77,14 +77,14 @@ movement_sensor = Pin(4, Pin.IN, Pin.PULL_UP)
 led = Pin(2, Pin.OUT)
 
 def sensor_get_values():
-  if movement_sensor.value() == 1: 
-    movement_value = "Motion captured"
-    led.value(1)
+  if movement_sensor.value() == 0: 
+    movement_value = "No Motion"
+    led.value(0)
     time.sleep(0.5)
     sleep(0.5)
   else:
-    movement_value = "No Motion"
-    led.value(0)
+    movement_value = "Motion captured"
+    led.value(1)
     time.sleep(0.5)
     sleep(0.5)
 
